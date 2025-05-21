83ce6920f6196afa9171676e8e161df2086866b2
Suriyakumar7373 <suryakumar2004sk@gmail.com>
2025-05-21 14:16:35 +0530

    Add files via upload

---
+++ day6/aar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int size,sum=0;
+    scanf("%d",&arr[i]);
+    int arr[size];
+    sum+=arr[i];
+    
+}
---
+++ day6/arrayodd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int size;
+    int even=0,odd=0;
+    printf("enter the size of the array:");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%")
+    }
+}
